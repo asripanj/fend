46a07e9721170eab6497580c97680cb96edde5a4
asripanj <vsaranje@gmail.com>
2020-12-18 17:27:07 -0500

    async test

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,7 +1,8 @@
-function handleSubmit(event) {
-    event.preventDefault()
+function handleSubmit(event) {    
+    if (document.getElementById('name') != null){
+      let formText = document.getElementById('name').value;
+    }
 
-      let formText = document.getElementById('name').value
       Client.postData('http://localhost:8081/getSentiment',{text: formText})
       .then(data=>{
           Client.updateUI(data) //update UI inside callback
