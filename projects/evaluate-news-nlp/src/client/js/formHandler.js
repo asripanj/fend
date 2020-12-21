4f61507149c64acbd0b6dc9179a81ea9bc7297ae
asripanj <vsaranje@gmail.com>
2020-12-21 14:03:54 -0500

    Working on regex

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -4,7 +4,15 @@ import {updateUI} from './updateUI'
 function handleSubmit(event) {    
   event.preventDefault();
       let formText = document.getElementById('name').value;
-      document.getElementById('text').innerHTML = 'Thank you';
+
+
+  var regex = new RegExp('^[a-zA-Z\\s]+$'); //letters and whitespace only
+  if(regex.test(formText){
+    document.getElementById('text').innerHTML = 'Thank you';
+  }
+  else{
+    document.getElementById('text').innerHTML = 'Invalid input - letters and whitespace only';
+  }
 
   postData('http://localhost:8081/getSentiment',{text: formText})
   .then(data=>{
